4407d036a17611c6a5a23b264c9c566afd7e578b
Nguyen Van <nguyen86v@gmail.com>
2023-05-24 09:03:26 -0700

    created shortcut command activate microphone permission from webpage

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "accessMicrophone") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { permission: "microphone" });
-    });
-  }
-});
+//this file will run in background an will process the data. here we will use command to request for microphone permission and record and process the audio file.
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.permission === "microphone") {
-    chrome.tabs.create({ url: "microphone-access.html" });
-  }
+chrome.commands.onCommand.addListener(() => {
+  console.log("command was pressed");
 });
